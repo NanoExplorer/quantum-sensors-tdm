52a3dc348e56ea22ad0f12383ffe781d688d9dcc
Christopher Rooney <christopher@rooneyworks.com>
2026-04-11 13:43:12 -0600

    Change some instances of sendReg to call then new rack_transport.write_wreg

--- cringe/DFBx2/dfbclkcard.py
+++ cringe/DFBx2/dfbclkcard.py
@@ -14,6 +14,7 @@ from . import dprcal
 from . import clkrap
 from cringe.shared import terminal_colors as tc
 from cringe.shared import log
+from cringe.shared.rack_transport import write_wreg
 
 
 class dfbclkcard(QWidget):
@@ -351,16 +352,8 @@ class dfbclkcard(QWidget):
 		self.sendReg(wregval)	
 		
 
-	def sendReg(self, wregval): 
-		log.debug(tc.COMMAND + "send to address", self.address, ":", tc.BOLD, wregval, tc.ENDC)
-		b0 = (wregval & 0x7f ) << 1			# 1st 7 bits shifted up 1
-		b1 = ((wregval >> 7) & 0x7f) <<  1	 # 2nd 7 bits shifted up 1
-		b2 = ((wregval >> 14) & 0x7f) << 1	 # 3rd 7 bits shifted up 1
-		b3 = ((wregval >> 21) & 0x7f) << 1	 # 4th 7 bits shifted up 1
-		b4 = (self.address << 1) + 1		   # Address shifted up 1 bit with address bit set
-		msg = struct.pack('BBBBB', b0, b1, b2, b3, b4)
-		self.serialport.write(msg)
-		time.sleep(0.001)
+	def sendReg(self, wregval):
+		write_wreg(self.serialport, wregval, self.address)
 		
 		
 	def packCARDglobals(self):
